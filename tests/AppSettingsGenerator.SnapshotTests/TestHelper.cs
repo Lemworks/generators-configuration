3eda497502812a5e6a302302f6e58daa82faf2e4
Thomas Lem <thomas@lemworks.com>
2025-05-13 19:09:43 +0200

    Remove duplicate source

--- tests/AppSettingsGenerator.SnapshotTests/TestHelper.cs
+++ tests/AppSettingsGenerator.SnapshotTests/TestHelper.cs
@@ -7,15 +7,9 @@ public static class TestHelper
 {
     public static Task Verify(string source)
     {
-        var syntaxTree = CSharpSyntaxTree.ParseText(source, CSharpParseOptions.Default);
-        
-        var compilation = CSharpCompilation.Create(
-            assemblyName: "Test",
-            syntaxTrees: [syntaxTree]);
-
         var driver = CSharpGeneratorDriver.Create(new AppSettingsGenerator())
             .AddAdditionalTexts([new InMemoryAdditionalText(Path.DirectorySeparatorChar + "appsettings.json", source)])
-            .RunGenerators(compilation);
+            .RunGenerators(CSharpCompilation.Create("Testing"));
 
         return Verifier.Verify(driver)
             .UseDirectory("Snapshots");
