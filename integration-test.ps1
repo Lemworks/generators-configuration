7e58cb88adce036abb885a64aa5f20c9bb3f3f19
Thomas Lem <thomas@lemworks.com>
2025-05-16 16:19:04 +0200

    Cleanup and create integration test package version

--- integration-test.ps1
+++ integration-test.ps1
@@ -1,3 +1,9 @@
+Remove-Item -Path ./artifacts/*.0.0.1-integration-test.nupkg -ErrorAction SilentlyContinue
+
+Remove-Item -Path ./packages/ -Recurse -ErrorAction SilentlyContinue
+
+dotnet pack -c Release -o ./artifacts -p:Version=0.0.1-integration-test
+
 dotnet restore ./tests/AppSettingsGenerator.NugetIntegrationTests --packages ./packages --configfile "nuget.integration-tests.config"
 
 dotnet build ./tests/AppSettingsGenerator.NugetIntegrationTests -c Release --packages ./packages --no-restore
