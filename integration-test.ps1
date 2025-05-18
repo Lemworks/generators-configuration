2a8722639e0d45d2d20f2ddd740f767f7c9e65e4
Thomas Lem <thomas@lemworks.com>
2025-05-18 22:28:44 +0200

    Fix path

--- integration-test.ps1
+++ integration-test.ps1
@@ -4,7 +4,7 @@ Remove-Item -Path .\packages\ -Recurse -ErrorAction SilentlyContinue
 
 dotnet pack -c Release -o .\artifacts -p:Version=0.0.1-integration-test
 
-dotnet restore .\tests\AppSettingsGenerator.NugetIntegrationTests --packages .\packages --configfile "nuget.integration-tests.config"
+dotnet restore .\tests\AppSettingsGenerator.NugetIntegrationTests --packages .\packages --configfile .\nuget.integration-tests.config
 
 dotnet build .\tests\AppSettingsGenerator.NugetIntegrationTests -c Release --packages .\packages --no-restore
 
