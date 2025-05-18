5ac8322954c8900555059b121c5b82b7552c223c
Thomas Lem <thomas@lemworks.com>
2025-05-18 22:16:52 +0200

    Use backslash

--- integration-test.ps1
+++ integration-test.ps1
@@ -1,11 +1,11 @@
-Remove-Item -Path ./artifacts/*.0.0.1-integration-test.nupkg -ErrorAction SilentlyContinue
+Remove-Item -Path .\artifacts\*.0.0.1-integration-test.nupkg -ErrorAction SilentlyContinue
 
-Remove-Item -Path ./packages/ -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path .\packages\ -Recurse -ErrorAction SilentlyContinue
 
-dotnet pack -c Release -o ./artifacts -p:Version=0.0.1-integration-test
+dotnet pack -c Release -o .\artifacts -p:Version=0.0.1-integration-test
 
-dotnet restore ./tests/AppSettingsGenerator.NugetIntegrationTests --packages ./packages --configfile "nuget.integration-tests.config"
+dotnet restore .\tests\AppSettingsGenerator.NugetIntegrationTests --packages .\packages --configfile "nuget.integration-tests.config"
 
-dotnet build ./tests/AppSettingsGenerator.NugetIntegrationTests -c Release --packages ./packages --no-restore
+dotnet build .\tests\AppSettingsGenerator.NugetIntegrationTests -c Release --packages .\packages --no-restore
 
-dotnet test ./tests/AppSettingsGenerator.NugetIntegrationTests -c Release --no-build --no-restore
+dotnet test .\tests\AppSettingsGenerator.NugetIntegrationTests -c Release --no-build --no-restore
