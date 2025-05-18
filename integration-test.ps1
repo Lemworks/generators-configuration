9bb59bfc160bf1b1ce849e5b46a4ef04962565d8
Thomas Lem <thomas@lemworks.com>
2025-05-18 22:56:38 +0200

    Use full csproj path

--- integration-test.ps1
+++ integration-test.ps1
@@ -4,8 +4,8 @@ Remove-Item -Path $PSScriptRoot\packages\ -Recurse -ErrorAction SilentlyContinue
 
 dotnet pack -c Release -o $PSScriptRoot\artifacts -p:Version=0.0.1-integration-test
 
-dotnet restore $PSScriptRoot\tests\AppSettingsGenerator.NugetIntegrationTests --packages $PSScriptRoot\packages --configfile $PSScriptRoot\nuget.integration-tests.config
+dotnet restore $PSScriptRoot\tests\AppSettingsGenerator.NugetIntegrationTests\AppSettingsGenerator.NugetIntegrationTests.csproj --packages $PSScriptRoot\packages --configfile $PSScriptRoot\nuget.integration-tests.config
 
-dotnet build $PSScriptRoot\tests\AppSettingsGenerator.NugetIntegrationTests -c Release --packages $PSScriptRoot\packages --no-restore
+dotnet build $PSScriptRoot\tests\AppSettingsGenerator.NugetIntegrationTests\AppSettingsGenerator.NugetIntegrationTests.csproj -c Release --packages $PSScriptRoot\packages --no-restore
 
-dotnet test $PSScriptRoot\tests\AppSettingsGenerator.NugetIntegrationTests -c Release --no-build --no-restore
+dotnet test $PSScriptRoot\tests\AppSettingsGenerator.NugetIntegrationTests\AppSettingsGenerator.NugetIntegrationTests.csproj -c Release --no-build --no-restore
